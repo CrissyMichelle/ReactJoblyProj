291deca2b6786fb3fc9355e3ae9032b5d45f6600
Crissy Cabrera <crissymichelle@proton.me>
2024-01-07 18:53:08 -1000

    added login signup and logoutfunctions to App.js

--- jobly-react/src/App.js
+++ jobly-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
@@ -11,19 +11,41 @@ import SignupRoute from './routes/SignupRoute';
 import AuthRoute from './routes/AuthRoute';
 
 import './App.css';
+import JoblyApi from './api';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  function login(token, username) {
+    JoblyApi.setToken(token);
+    setIsLoggedIn(true);
+    setUsername(username);
+  }
+
+  function logout() {
+    JoblyApi.setToken(null);
+    setIsLoggedIn(false);
+    setUsername('');
+  }
+
+  function signup(token, username) {
+    JoblyApi.setToken(token);
+    setIsLoggedIn(true);
+    setUsername(username);
+  }
+
   return (
     <Router>
-      <NavBar />
+      <NavBar isLoggedIn={isLoggedIn} username={username} logout={logout} />
       <Routes>
+        <Route path="/login" element={<AuthRoute login={login} />} />
+        <Route path="/signup" element={<SignupRoute signup={signup} />} />
         <Route path="/" element={<HomeRoute />} />
         <Route path="/companies" element={<CompaniesRoute />} />
         <Route path="/companies/:handle" element={<CompanyDetailRoute />} />
         <Route path="/jobs" element={<JobsRoute />} />
         <Route path="/profile" element={<ProfileRoute />} />
-        <Route path="/login" element={<AuthRoute />} />
-        <Route path="/signup" element={<SignupRoute />} />
       </Routes>
     </Router>
   );
--- jobly-react/src/components/NavBar.js
+++ jobly-react/src/components/NavBar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-function NavBar() {
+function NavBar({ isLoggedIn, username, logout }) {
     return (
         <nav>
             <Link to="/">Home</Link>
             <Link to="/companies">Companies</Link>
             <Link to="/jobs">Jobs</Link>
-            <Link to="/profile">Profile</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            {isLoggedIn ? (
+                <>
+                    <Link to="/profile">Profile</Link>
+                    <span>Welcome, {username}</span>
+                    <button onClick={logout}>Logout</button> 
+                </>
+            ) : (
+                <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Signup</Link>
+                </>
+            )}
         </nav>
     );
 }
