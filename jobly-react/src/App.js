0c5e82af56952ab304fd3f864437417b199d0a9a
Crissy Cabrera <crissymichelle@proton.me>
2024-01-07 19:34:15 -1000

    changed App.js to manage token and currentUser state

--- jobly-react/src/App.js
+++ jobly-react/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import jwtDecode from 'jwt-decode';
 import NavBar from './components/NavBar';
 import HomeRoute from './routes/HomeRoute';
 import CompaniesRoute from './routes/CompaniesRoute';
@@ -9,45 +9,53 @@ import JobsRoute from './routes/JobsRoute';
 import ProfileRoute from './routes/ProfileRoute';
 import SignupRoute from './routes/SignupRoute';
 import AuthRoute from './routes/AuthRoute';
-
-import './App.css';
 import JoblyApi from './api';
+import './App.css';
+
+export const AuthContext = React.createContext();
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('authToken') || null);
+  const [currentUser, setCurrentUser] = useState('');
 
-  function login(token, username) {
-    JoblyApi.setToken(token);
-    setIsLoggedIn(true);
-    setUsername(username);
+  useEffect(() => {
+    if (token) {
+      const user = jwtDecode(token);
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(null);
+    }
+  }, [token]);
+
+  function login(token) {
+    setToken(token);
   }
 
   function logout() {
-    JoblyApi.setToken(null);
-    setIsLoggedIn(false);
-    setUsername('');
+    setToken(null);
   }
 
-  function signup(token, username) {
-    JoblyApi.setToken(token);
-    setIsLoggedIn(true);
-    setUsername(username);
+  function signup(token) {
+    setToken(token);
   }
+
+  const isLoggedIn = !!token; // true if token is null=false
 
   return (
-    <Router>
-      <NavBar isLoggedIn={isLoggedIn} username={username} logout={logout} />
-      <Routes>
-        <Route path="/login" element={<AuthRoute login={login} />} />
-        <Route path="/signup" element={<SignupRoute signup={signup} />} />
-        <Route path="/" element={<HomeRoute />} />
-        <Route path="/companies" element={<CompaniesRoute />} />
-        <Route path="/companies/:handle" element={<CompanyDetailRoute />} />
-        <Route path="/jobs" element={<JobsRoute />} />
-        <Route path="/profile" element={<ProfileRoute />} />
-      </Routes>
-    </Router>
+    <AuthContext.Provider value={{ token, setToken, currentUser, login, logout, signup }}>
+      <Router>
+        <NavBar isLoggedIn={isLoggedIn} username={currentUser?.username} logout={logout} />
+        <Routes>
+          <Route path="/login" element={<AuthRoute login={login} />} />
+          <Route path="/signup" element={<SignupRoute signup={signup} />} />
+          <Route path="/" element={<HomeRoute />} />
+          <Route path="/companies" element={<CompaniesRoute />} />
+          <Route path="/companies/:handle" element={<CompanyDetailRoute />} />
+          <Route path="/jobs" element={<JobsRoute />} />
+          <Route path="/profile" element={<ProfileRoute />} />
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
